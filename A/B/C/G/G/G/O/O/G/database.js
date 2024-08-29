965bf815d785f7cb41a763e93145dd3d847fd364
Mʀ Iꜱᴜʀᴜ X <156693573+MrIsuru@users.noreply.github.com>
2024-08-29 19:06:17 +0530

    Update database.js

--- A/B/C/G/G/G/O/O/G/database.js
+++ A/B/C/G/G/G/O/O/G/database.js
@@ -1,6 +1,3 @@
-[{
-"aliveimg":  "https://i.ibb.co/pwNf08R/1713103024643.jpg"
-},
 {
-"alivemsg": "*Hello, I am Alive Now!*\n\n*👽 Bᴏᴛ Nᴀᴍᴇ - MACTRIX-MD*\n*👨‍💻 Bᴏᴛ Dᴇᴠ - Isuru Chamika*\n*🕵️ Bᴏᴛ Oᴡɴᴇʀ - config.OWNER_NAME*\n\n🟢 I am Online Still uptime\n🔮 You Can Use Me With This [ config.PREFIX ] Prefix.\n🔁 Response Delay :\n🖥️ Host : host\n🧬 Version : 1.0.0\n\n*You Can Contact Bot Developer Using,*\n*◦config.PREFIXdev*\n*◦config.PREFIXdeveloper*\n*◦config.PREFIXisuru*\n*◦config.PREFIXtharu*\n*Commands..!*\n\n🎛️ Work Mode : config.MODE\n\n\nfooter"
-}]
+"aliveimg":  "https://i.ibb.co/pwNf08R/1713103024643.jpg"
+}
