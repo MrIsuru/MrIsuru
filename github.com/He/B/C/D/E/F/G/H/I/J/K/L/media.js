12a15bd9629cfe4043f7e2649a906b3cf244becb
Mʀ Iꜱᴜʀᴜ X <156693573+MrIsuru@users.noreply.github.com>
2024-08-30 15:06:45 +0530

    ᴄᴏɴꜰɪɢ ꜰɪʟᴇꜱ ꜰᴏʀ ᴍʏ ɢɪᴛʜᴜʙ ᴘʀᴏꜰɪʟᴇ

---
+++ github.com/He/B/C/D/E/F/G/H/I/J/K/L/media.js
@@ -0,0 +1,6 @@
+{
+"aliveimg": "https://i.ibb.co/pwNf08R/1713103024643.jpg",
+"menuimg": "https://i.ibb.co/pwNf08R/1713103024643.jpg",
+"devimg": "https://i.ibb.co/pwNf08R/1713103024643.jpg",
+"tharuimg": "https://i.ibb.co/pwNf08R/1713103024643.jpg"
+}
